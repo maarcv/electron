dbcc4f260fffd9459b979eb39c26e7f980aa3979
Marc <mrcvilardebo@gmail.com>
2025-09-25 16:04:24 +0200

    afegides var entorn

--- main.js
+++ main.js
@@ -10,7 +10,16 @@ if (app.isPackaged) {
   dotenv.config() // .env a l’arrel del repo en desenvolupament
 }
 
-function createSplash(apiRoute,  apiSyncroRoute, terminalId, url) {
+const apiRoute = process.env.API_ROUTE
+const apiSyncroRoute = process.env.API_SYNCRO_ROUTE
+const terminalId = process.env.TERMINAL_ID
+const customWeight = process.env.CUSTOM_WEIGHT
+const registerLogs = process.env.REGISTER_LOGS
+const keyboard = process.env.KEYBOARD
+
+const url = `https://operations.abianchini.es:4043?API_ROUTE=${apiRoute}&API_SYNCRO_ROUTE=${apiSyncroRoute}&TERMINAL_ID=${terminalId}&CUSTOM_WEIGHT=${customWeight}&REGISTER_LOGS=${registerLogs}&KEYBOARD=${keyboard}`
+
+function createSplash() {
   const splash = new BrowserWindow({
       fullscreen: true,
       frame: false,
@@ -21,13 +30,16 @@ function createSplash(apiRoute,  apiSyncroRoute, terminalId, url) {
 
   const html = `
     <html>
-      <body>
+        <body>
         <h2>Carregant...</h2>
-        <p><b>API_ROUTE:</b> ${apiRoute}</p>
-        <p><b>API_SYNCRO_ROUTE:</b> ${apiSyncroRoute}</p>
-        <p><b>TERMINAL_ID:</b> ${terminalId}</p>
-        <p><b>TERMINALS_URL:</b> ${url}</p>
-      </body>
+            <p><b>API_ROUTE:</b> ${apiRoute}</p>
+            <p><b>API_SYNCRO_ROUTE:</b> ${apiSyncroRoute}</p>
+            <p><b>TERMINAL_ID:</b> ${terminalId}</p>
+            <p><b>TERMINALS_URL:</b> ${url}</p>
+            <p><b>CUSTOM_WEIGHT:</b> ${customWeight}</p>
+            <p><b>REGISTER_LOGS:</b> ${registerLogs}</p>
+            <p><b>KEYBOARD:</b> ${keyboard}</p>
+        </body>
     </html>
   `
 
@@ -35,7 +47,7 @@ function createSplash(apiRoute,  apiSyncroRoute, terminalId, url) {
   return splash
 }
 
-function createMainWindow(url) {
+function createMainWindow() {
   const win = new BrowserWindow({
     fullscreen: true,
     frame: false,
@@ -48,10 +60,6 @@ function createMainWindow(url) {
 }
 
 app.whenReady().then(() => {
-  const apiRoute = process.env.API_ROUTE
-  const apiSyncroRoute = process.env.API_SYNCRO_ROUTE
-  const terminalId = process.env.TERMINAL_ID
-  const url = `https://operations.abianchini.es:4043?API_ROUTE=${apiRoute}&API_SYNCRO_ROUTE=${apiSyncroRoute}&TERMINAL_ID=${terminalId}`
 
   // Mostra splash
   const splash = createSplash(apiRoute, apiSyncroRoute, terminalId, url)
