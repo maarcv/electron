e2c09daae6adf623f903c650df77376cfcabe926
Marc <mrcvilardebo@gmail.com>
2025-09-25 12:06:43 +0200

    continua

--- main.js
+++ main.js
@@ -1,15 +1,20 @@
 const { app, BrowserWindow } = require('electron/main')
 const path = require('node:path')
+require('dotenv').config() 
 
 function createWindow () {
+    const terminalId = process.env.terminalId
+    const personId = process.env.personId
   const win = new BrowserWindow({
-    width: 800,
-    height: 600,
+      fullscreen: true,         // Obre directament en pantalla completa
+      frame: false,             // Treu la barra de Windows (sense controls)
+      autoHideMenuBar: true,    // Oculta la barra de menú si n’hi hagués
     webPreferences: {
       preload: path.join(__dirname, 'preload.js')
     }
   })
-  win.loadURL('https://families.development.moltanimal.com')
+    const url = `https://operations.abianchini.es:4043?person=${personId}&terminal=${terminalId}`
+    win.loadURL(url)
 }
 
 app.whenReady().then(() => {
